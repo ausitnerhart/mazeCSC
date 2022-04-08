34136c377ab0d820eac1bfa2ec8c9d41db7d2da5
ealberty <101431003+ealberty@users.noreply.github.com>
2022-04-08 14:22:18 -0500

    Add files via upload

---
+++ maze/config.py
@@ -0,0 +1,36 @@
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+TILE_SIZE = 32
+FPS = 60
+NUM_TROLLS = 20
+
+CHARACTER_LEVEL = 3
+WALL_LEVEL = 2
+FLOOR_LEVEL = 1
+
+GUY_VEL = 3
+TROLL_VEL = 3
+
+DIRECTIONS = ['up', 'down', 'left', 'right']
+
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+
+MAZE = [
+    'WWWWWWWWWWWWWWWWWWWW',
+    'W..................W',
+    'W........G.........W',
+    'W...WWW............W',
+    'W..................W',
+    'W...............W..W',
+    'W...WWWWWWWWWWWWW..W',
+    'W...W...........W..W',
+    'W...W.W.WWWWWWW.W..W',
+    'W...W.W.W.......W..W',
+    'W...W.W.WWWWW...W..W',
+    'W...W.W.....W...W..W',
+    'W...W.WWWWW.W...W..W',
+    'W.........W.W......W',
+    'WWWWWWWWWWWWWWWWWWWW',
+]
